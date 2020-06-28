b17718d0917c8228932142eb7434ac8882297b07
time4cuppa <espresso@timeforacuppa.com>
2020-06-28 14:12:57 +1000

    Fixed anothe typo -surely there's an auto way for this...

--- network/cloud/synology-drive/actions.py
+++ network/cloud/synology-drive/actions.py
@@ -11,7 +11,7 @@ IgnoreAutodep = True
 
 def setup():
     shelltools.system("ar xf synology-drive-client-11078.x86_64.deb")
-    shelltools.system("tar xf data.tar.gz")
+    shelltools.system("tar xf data.tar.xz")
 
 def install():
     shutil.rmtree("usr/lib/nautilus/extensions-3.0/")
